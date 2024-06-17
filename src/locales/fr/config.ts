a43051bac14db5e23be104644a5f86ba9a8a65f1
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-06-17 15:00:32 +0200

    Translate pokemon summary to fr

--- src/locales/fr/config.ts
+++ src/locales/fr/config.ts
@@ -39,6 +39,7 @@ import { tutorial } from "./tutorial";
 import { voucher } from "./voucher";
 import { weather } from "./weather";
 import { partyUiHandler } from "./party-ui-handler";
+import { pokemonSummary } from "./pokemon-summary";
 
 export const frConfig = {
   ability: ability,
@@ -73,6 +74,7 @@ export const frConfig = {
   pokemon: pokemon,
   pokemonInfo: pokemonInfo,
   pokemonInfoContainer: pokemonInfoContainer,
+  pokemonSummary: pokemonSummary,
   saveSlotSelectUiHandler: saveSlotSelectUiHandler,
   splashMessages: splashMessages,
   starterSelectUiHandler: starterSelectUiHandler,
---
+++ src/locales/fr/pokemon-summary.ts
@@ -0,0 +1,17 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const pokemonSummary: SimpleTranslationEntries = {
+  "pokemonInfo": "Info Pokémon",
+  "status": "Status",
+  "powerAccuracyCategory": "Puissance\nPrécision\nCatégorie",
+  "type": "Type",
+  "unknownTrainer": "Inconnu",
+  "ot": "DO",
+  "luck": "Chance",
+  "nature": "de nature",
+  "apparently": "apparemment",
+  "metAtLv": "rencontré au N.",
+  "expPoints": "Points Exp.",
+  "nextLv": "Niveau suiv.",
+  "cancel": "Annuler",
+} as const;
